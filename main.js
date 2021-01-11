a7378504ce47bf696fcfc2a630c2a15dd05f43f0
tashankeh <75678349+tashankeh@users.noreply.github.com>
2021-01-11 02:44:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const returnRandBase = () => {
   return dnaBases[Math.floor(Math.random() * 4)] 
 }
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = []
   for (let i = 0; i < 15; i++) {
@@ -12,54 +12,52 @@ const mockUpStrand = () => {
   }
   return newStrand
 }
-// factory function to create multiple p.aequor
-const pAequorFactory = (specimentNum,dna) => {
+
+// factory function to create multiple p.aequor speciments
+const pAequorFactory = (specimenNum,dna) => {
   return{
-  specimentNum: specimentNum,
+  specimenNum: specimenNum,
   dna:dna,
+  //specimen's method to mutate a randomly selected base of its DNA strand
   mutate() {
-    let randomBaseIndex = Math.floor(Math.random() * 16);
-    //console.log(`random dan index is ${randomBaseIndex}`);
-    let randomBase = returnRandBase();
-    //change the base of the current strand at the random index
-    //with the new random base 
-    this.dna.splice(randomBaseIndex,0,randomBase);
+    let index = Math.floor(Math.random()*16);
+    this.dna.splice(index,0,returnRandBase()); 
     return dna;
   },
-  //compare dna sequences of different P. aequor
+
+  //specimen's method to compare its own dna sequences with that of a different P. aequor specimen
   compareDNA(pAequor) {
-    let sameBaseCount = 0;
-    let strandLength = dna.length;
-    for (i=0; i<strandLength;i++){
+    let identicalBaseCount = 0;
+    for (i=0; i<dna.length;i++){
       if (this.dna[i] === pAequor.dna[i]) {
-        sameBaseCount++;
+        identicalBaseCount ++;
       }
     }
-    let msg = `${this.specimentNum} and ${pAequor.specimentNum} have ${((sameBaseCount/strandLength)*100).toFixed(1)} % DNA in commom`;
+    let msg = `Specimen:${this.specimenNum} and Specimen:${pAequor.specimenNum} have ${((identicalBaseCount /dna.length)*100).toFixed(1)} % DNA in commom`;
     return msg;
   },
-  //Is this pAequor likely to survive?
+
+  //willLikelySurvive() determines if specimen is likely to survive
   willLikelySurvive() {
     let baseCount = 0;
-    let dnaLength= this.dna.length;
-    this.dna.forEach(base => {
-      if ((base === 'C') || (base === 'G')) {
+    this.dna.forEach(DNAbase => {
+      if ((DNAbase === 'C') || (DNAbase === 'G')) {
         baseCount++;
       }
     })
-   let survival = ((baseCount/dnaLength)*100).toFixed(1)
-   if (survival >= 60) {
+   let survival = ((baseCount/this.dna.length)*100).toFixed(1);
+    if (survival >= 60) {
        return true;
-      } else {
+    } else {
         return false;
-      }
-  },
-  //complementary DNA Strand function
-  complementStrand() {
-    let complementaryDNA = [];
+    }
+    },
 
-    for (i=0; i<this.dna.length;i++) {
-      switch (this.dna[i]) {
+  //specimen's method to auto create its own complementary DNA Strand
+   complementStrand() {
+    let complementaryDNA = [];
+    for (const base of this.dna) {
+      switch (base) {
         case 'A':
         complementaryDNA.push('T');
         break;
@@ -74,49 +72,65 @@ const pAequorFactory = (specimentNum,dna) => {
         break;
       }
     }
-    //console.log(complementaryDNA);
     return complementaryDNA;
-
   }
   };
-}
-let pAequorSpecimens = [];
-let count = 1;
-while (count<=30) {
-  let tempDNAStrand = mockUpStrand();
-  let org= pAequorFactory(count,tempDNAStrand);
+}// end of factory function
 
-  if (org.willLikelySurvive() === true){
-      pAequorSpecimens.push(org);
+// function to generate a desired number of specimens that are more likely to survive 
+let pAequorSpecimens = [];//holds the specimens
+const createSpecimens = number =>{
+let count = 1;
+while (count<=number) {
+  let specimen= pAequorFactory(count,mockUpStrand());
+  if (specimen.willLikelySurvive() === true){
+      pAequorSpecimens.push(specimen);
       count++;
   }
 }
-//build complementary speciments of pAeaquor from AequorSpecimens
+return pAequorSpecimens;
+}
 
-let comAequorSpecimens = [];
-for(j=0; j<30; j++){
+//function to build complementary specimens from existing ones
+let compAequorSpecimens = []; //holds the complementary specimens
+const createComSpecimens = baseSpecimens => {
+  for(const specimen of baseSpecimens){
+    compAequorSpecimens.push(pAequorFactory(specimen.specimenNum,specimen.complementStrand()));
+    }
+  return compAequorSpecimens;
+}
 
-  let comDNA = pAequorSpecimens[j].complementStrand();
-  comAequorSpecimens.push(pAequorFactory(j,comDNA));
+//compare DNA of speciments
+const compareSpecimensDNA = (specimensx) =>{
+for (const strandx of specimensx) {
+    for (const strandy of specimensx){
+        if (strandx !== strandy){
+         console.log(strandx.compareDNA(strandy));
+        }
+    }
+}
+};
+
+//log speciments from pAequorSpecimens array of speciments to console
+const logToConsole = (specimensArray, num) =>{ 
+  if (specimensArray === pAequorSpecimens) {
+    createSpecimens(num);
+    specimensArray.forEach(specimen => {
+    console.log(`pAequorSpecimens: Specimen ${specimen.specimenNum}: DNA: ${specimen['dna']}`);
+  });}
+  if (specimensArray === compAequorSpecimens) {
+    createSpecimens(num);
+    createComSpecimens(pAequorSpecimens);
+    specimensArray.forEach(specimen => {
+    console.log(`compAequorSpecimens: Specimen ${specimen.specimenNum}: DNA: ${specimen['dna']}`);
+  });}
   }
 
-//log 30 speciments from pAequorSpecimens array of speciments to console
-logToConsole = (specimentArray) =>{ 
-specimentArray.forEach(specimen => {
-  console.log(`Specimen ${specimen['specimentNum']}: DNA: ${specimen['dna']}`)
-})};
+
 // tests
-logToConsole(pAequorSpecimens);
-//logToConsole(comAequorSpecimens);
- 
 
-
-
-
-
-
-
-
-
-
-
+logToConsole(pAequorSpecimens,5); // Donot comment out this while testing the other functionalities below
+//logToConsole(compAequorSpecimens,05);
+//console.log(pAequorSpecimens[0].mutate()); //works
+//console.log(pAequorSpecimens[0].compareDNA(pAequorSpecimens[1])); // works
+//compareSpecimensDNA(pAequorSpecimens); works
